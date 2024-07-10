83bdae2ed58e85768427efb845cf3c5282401c2e
Annacletter <anna.cletter12@gmail.com>
2024-07-10 15:19:13 -0700

    Implemented the Navbar componet

--- dashboard/innreserve/src/App.js
+++ dashboard/innreserve/src/App.js
@@ -1,10 +1,17 @@
+// Import statements
 import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
 
+// Functional component App
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome to InnReserve</h1>
-      {/* Add your components here */}
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <h1>Welcome to InnReserve</h1>
+        {/* Add more components or content here */}
+      </div>
     </div>
   );
 }
