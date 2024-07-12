76170bac3e1bfa31c31f5f6b56d566afbcba308f
Lulekoh <lulekoleoquintus12@gmail.com>
2024-07-12 14:14:34 -0700

    react regeneration

--- dashboard/innreserve/src/App.js
+++ dashboard/innreserve/src/App.js
@@ -30,7 +30,6 @@ import image1 from './images/image1.jpg';
 import image2 from './images/image2.jpg';
 import image3 from './images/image3.jpg';
 // Import statements
-import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 
