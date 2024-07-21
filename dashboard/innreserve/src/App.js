f824a8d9f526e987a47b20b0d8dca9362b49935d
Annacletter <anna.cletter12@gmail.com>
2024-07-21 12:45:56 -0700

    Updated App file

--- dashboard/innreserve/src/App.js
+++ dashboard/innreserve/src/App.js
@@ -13,6 +13,7 @@ import './App.css';
 import image1 from './images/image1.jpg';
 import image2 from './images/image2.jpg';
 import image3 from './images/image3.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const basename = '/InnReserve-Booking-System';
 
@@ -102,7 +103,6 @@ function App() {
         </Switch>
       </Router>
     </div>
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
