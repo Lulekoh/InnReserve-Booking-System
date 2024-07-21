e7cc54e356f86608d9a25454ef49102334e1b7bf
Annacletter <anna.cletter12@gmail.com>
2024-07-21 14:21:38 -0700

    included image imports

--- dashboard/innreserve/src/App.js
+++ dashboard/innreserve/src/App.js
@@ -11,6 +11,9 @@ import BookingState from './context/BookingState';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
 
 const basename = '/InnReserve-Booking-System';
 
